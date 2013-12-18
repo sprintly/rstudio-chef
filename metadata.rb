1c3221e5acd5763b6ccfdae3a88291c27362f7d8
Joe Stump <joe@joestump.net>
2013-12-18 14:41:31 -0800

    Upped the version to 0.3.0.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'joe@joestump.net'
 license          'All rights reserved'
 description      'Installs/Configures rstudio'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.0'
+version          '0.3.0'
 
 depends "apt"
 depends "nginx"
