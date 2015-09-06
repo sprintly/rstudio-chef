c74293c621ea950e3a8f99fc0a3c51a061e3c11a
David F. Severski <davidski@deadheaven.com>
2015-09-06 09:22:52 -0700

    Document supprot for platforms/add chef-sugar dependency

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,9 @@ license          'All rights reserved'
 description      'Installs/Configures rstudio'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.3.0'
+supports         'ubuntu', '>= 12.04'
+supports         'redhat'
+supports				 'amazon'
 
 depends "apt"
 depends "nginx"
