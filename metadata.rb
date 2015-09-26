4406baf76eca05a40599291c1c047ac26f7043c4
davidski <davidski@deadheaven.com>
2015-09-26 05:46:31 -0700

    Add source and issues url support.

--- metadata.rb
+++ metadata.rb
@@ -5,10 +5,12 @@ license          'All rights reserved'
 description      'Installs/Configures rstudio'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.3.0'
+source_url       'https://github.com/sprintly/rstudio-chef' if respond_to?(:source_url)
+issues_url       'https://github.com/sprintly/rstudio-chef/issues' if respond_to?(:issues_url)
 supports         'ubuntu', '>= 12.04'
 supports         'redhat'
 supports         'centos'
-supports				 'amazon'
+supports         'amazon'
 
 depends "apt"
 depends "nginx"
