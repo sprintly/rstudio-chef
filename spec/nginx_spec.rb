8a28f7a111e8efe0880a0b348549f5f8eb3c33e4
Nicholas Serra <nicks@ydekproductions.com>
2015-12-22 02:00:27 -0500

    Update tests

--- spec/nginx_spec.rb
+++ spec/nginx_spec.rb
@@ -2,9 +2,13 @@ require 'spec_helper'
 
 describe 'rstudio::nginx' do
   let(:chef_run) do
-    runner = ChefSpec::Runner.new(platform: 'ubuntu', version: '12.04')
+    runner = ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '12.04')
     runner.node.set['rstudio']['nginx']['server_name'] = 'test.example.com'
     runner.converge(described_recipe)
+  end
+
+  before(:each) do 
+    stub_command('which nginx').and_return(true)
   end
 
   it 'should create a knife.rb' do
--- spec/pam_spec.rb
+++ spec/pam_spec.rb
@@ -2,20 +2,9 @@ require 'spec_helper'
 
 describe 'rstudio::pam' do
   let(:chef_run) do
-    runner = ChefSpec::Runner.new platform: 'ubuntu', version: '12.04' do |node|
+    runner = ChefSpec::SoloRunner.new platform: 'ubuntu', version: '12.04' do |node|
       node.set['rstudio']['nginx']['server_name'] = 'test.example.com'
     end.converge(described_recipe)
-  end
-
-  before(:each) do 
-    ChefSpec::Server.create_data_bag('users', {
-      'jstump' => {
-        :id => 'jstump',
-        :rstudio => {
-          :passwd => 'secret-stuff'
-        }
-      }
-    })
   end
 
   it('includes rstudio::server') do 
