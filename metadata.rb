b8cc4e348e3f8a9aa109442e8c4b42b7eb1f6a12
Nicholas Serra <nicks@ydekproductions.com>
2016-01-01 02:58:30 -0500

    Version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'joe@joestump.net'
 license          'BSD'
 description      'Installs/Configures rstudio'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.3.0'
+version          '0.3.1'
 
 depends "apt"
 depends "nginx"
