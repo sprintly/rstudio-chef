1a8fbec67f2702859fe079fc810bfd6473fc25a4
David F. Severski <davidski@deadheaven.com>
2019-02-25 18:26:29 -0800

    Modernize metadata and freshen dependencies

--- metadata.rb
+++ metadata.rb
@@ -5,16 +5,17 @@ maintainer_email 'joe@joestump.net'
 license          'MIT'
 description      'Installs/Configures rstudio'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.4.0'
+version          '0.5.0'
 source_url       'https://github.com/sprintly/rstudio-chef' if respond_to?(:source_url)
 issues_url       'https://github.com/sprintly/rstudio-chef/issues' if respond_to?(:issues_url)
-supports         'ubuntu', '>= 12.04'
-supports         'redhat'
+
+supports         'ubuntu', '>= 16.04'
+supports         'debian'
 supports         'centos'
 supports         'amazon'
 
-depends "apt"
-depends "nginx"
-depends "r"
-depends "chef-sugar"
-depends "yum-epel"
+depends          "apt"
+depends          "chef-sugar"
+depends          "nginx"
+depends          "r", '~> 0.4.0'
+depends          "yum-epel"
